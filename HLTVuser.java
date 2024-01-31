2911cb0ff52efec3935af9c4b3e5c577ef52e841
Danial <qqteam8@gmail.com>
2024-01-31 13:44:01 +0600

    extend User class with premium HLTV user

--- HLTVuser.java
+++ HLTVuser.java
@@ -1,6 +1,13 @@
 class HLTVuser extends User {
-    private linkToProfile String;
-    HLTVuser() {
-        
+    private String linkToProfile;
+    HLTVuser(String nickname, String linkToProfile) {
+        super(nickname);
+        this.changeLink(linkToProfile);
+    }
+    public String getLink() {
+        return this.linkToProfile;
+    }
+    public void changeLink(String linkToProfile) {
+        this.linkToProfile = linkToProfile;
     }
 }
